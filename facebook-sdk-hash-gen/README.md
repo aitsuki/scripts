6403782834636579a76bd71535a7a3cb342937d9
aitsuki <aitsukigo@gmail.com>
2025-01-09 16:00:56 +0800

    完善 Facebook SDK 散列值生成脚本，添加命令行参数支持

--- facebook-sdk-hash-gen/README.md
+++ facebook-sdk-hash-gen/README.md
@@ -1,3 +1,9 @@
 # Facebook sdk hash generator
 
 用于生成 Android Facebook SDK 所需的签名密钥散列
+
+使用方式：
+
+```shell
+python facebook_sdk_hash_gen.py B7:9C:82:3E:E8:3C:9C:62:....
+```
--- facebook-sdk-hash-gen/facebook_sdk_hash_gen.py
+++ facebook-sdk-hash-gen/facebook_sdk_hash_gen.py
@@ -1,8 +1,19 @@
 import base64
+import argparse
 
-# 获取 sha1_hex_string 的方式：
-# GooglePlay Console -> 设置 -> 应用完整性 -> 应用签名 -> SHA-1 证书指纹
-sha1_hex_string = "4C:A4:2C:BA:3E:9D:F4..."
-sha1_bytes = bytes.fromhex(sha1_hex_string.replace(":", ""))
-facebook_hash = base64.b64encode(sha1_bytes).decode("utf-8")
-print(facebook_hash)
+
+def main():
+    parser = argparse.ArgumentParser(description="Process SHA1 hex string.")
+    parser.add_argument("sha1_hex_string", type=str, help="SHA1 hex string to process")
+    args = parser.parse_args()
+
+    sha1_hex_string = args.sha1_hex_string
+    # 处理 sha1_hex_string 的逻辑
+    print(f"Processing SHA1 hex string: {sha1_hex_string}")
+    sha1_bytes = bytes.fromhex(sha1_hex_string.replace(":", ""))
+    facebook_hash = base64.b64encode(sha1_bytes).decode("utf-8")
+    print(facebook_hash)
+
+
+if __name__ == "__main__":
+    main()
